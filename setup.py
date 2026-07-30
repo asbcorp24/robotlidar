c0639984638c6a8cefca115ca53b7aa7015c134a
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-07-30 08:34:48 +0300

    Устанавливать инструкцию USB-платы вместе с пакетом

--- setup.py
+++ setup.py
@@ -9,7 +9,10 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml', 'WEB.md', 'OFFLINE.md']),
+        (
+            'share/' + package_name,
+            ['package.xml', 'WEB.md', 'OFFLINE.md', 'STL19P_USB_BOARD.md'],
+        ),
         ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
         ('share/' + package_name + '/config', glob('config/*.yaml')),
         ('share/' + package_name + '/web/static', glob('web/static/*')),
