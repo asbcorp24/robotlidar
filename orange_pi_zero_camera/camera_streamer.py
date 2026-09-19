19b46550f248bf8589e5cdb1b831e7101f5b17cb
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-09-19 22:01:07 +0300

    Report actual active camera from Orange Pi

--- orange_pi_zero_camera/camera_streamer.py
+++ orange_pi_zero_camera/camera_streamer.py
@@ -363,6 +363,7 @@ class CameraStreamer:
             "pan_cdeg": self.pan_cdeg,
             "tilt_cdeg": self.tilt_cdeg,
             "link_mbps": 100,
+            "active_camera": self.active_camera,
         }
         try:
             status, data = self.json_request("POST", url, payload)
